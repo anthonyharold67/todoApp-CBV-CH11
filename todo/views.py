dde3a6d28ef94918c77f01dc659780fabad50ec1
AnthonyHarold67 <anthonyharold67@gmail.com>
2022-08-30 21:53:26 +0300

    finished

--- todo/views.py
+++ todo/views.py
@@ -6,18 +6,19 @@ from .forms import TodoForm
 from django.contrib import messages
 
 #cbv
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from django.urls import reverse_lazy
 
-def home(request):
-    todos = Todo.objects.all().order_by('-priority')
-    form = TodoForm()
-    
-    context = {
-        "todos" : todos,
-        "form" : form
-    }
-    return render(request, "todo/home.html", context)
+# def home(request):
+#     todos = Todo.objects.all().order_by('-priority')
+#     form = TodoForm()
+#     my_name="sd"
+#     context = {
+#         "todos" : todos,
+#         "form" : form,
+#         'my_name': my_name
+#     }
+#     return render(request, "todo/home.html", context)
 
 class TodoList(ListView):
     model = Todo
@@ -32,7 +33,18 @@ class TodoCreate(CreateView):
     model = Todo
     form_class = TodoForm
     template_name = "todo/todo_add.html" #default u todo/todo_form.html
-    success_url = reverse_lazy("list")
+    success_url = reverse_lazy("list")#reverse_lazy functionlardaki redirectin yerine kullanılıyor
+
+class TodoCreateList(CreateView):
+    model = Todo
+    form_class = TodoForm
+    template_name = "todo/home.html" #default u todo/todo_form.html
+    success_url = reverse_lazy("home")#reverse_lazy functionlardaki redirectin yerine kullanılıyor 
+
+    def get_context_data(self,**kwargs):
+        kwargs['todos'] = Todo.objects.order_by('-priority')  
+        kwargs['done_count'] = Todo.objects.filter(is_done=True).count()
+        return super(TodoCreateList,self).get_context_data(**kwargs)
 
 
 
@@ -68,6 +80,13 @@ def todo_update(request, id):
     }
     return render(request, "todo/todo_update.html", context)
 
+class TodoUpdate(UpdateView):
+    model = Todo
+    form_class= TodoForm
+    template_name= 'todo/todo_update.html' # Default todo/todo_update_form.html'
+    # pk_url_kwarg = 'id' #defaultu pk 
+    success_url = reverse_lazy("home")
+
 
 def todo_delete(request, id):
     todo = Todo.objects.get(id=id)
@@ -81,3 +100,17 @@ def todo_delete(request, id):
         "todo": todo
     }
     return render(request, "todo/todo_delete.html", context)
+
+class TodoDelete(DeleteView):
+    model=Todo
+    template_name = 'todo/todo_delete.html'
+    # default fscohort/student_confirm_delete.html
+    success_url = reverse_lazy("home")
+
+
+
+def is_completed(request,id):
+    todo=Todo.objects.get(id=id)
+    todo.is_done = not(todo.is_done)
+    todo.save()
+    return redirect('home')
